74d0ab8656c05a7e82bf0cf7b2fc1965e95690d1
Meraj <meraccos@unist.ac.kr>
2023-10-03 16:21:38 +0900

    background color changed

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const modeText = document.getElementById('mode-text');
 const body = document.body;
+const h1 = document.h1
 
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked) {
         body.classList.add('dark-mode');
+        h1.classList.add('dark-mode')
         modeText.textContent = 'Dark Mode';
     } else {
         body.classList.remove('dark-mode');
+        h1.classList.remove('dark-mode')
         modeText.textContent = 'Light Mode';
     }
 });
