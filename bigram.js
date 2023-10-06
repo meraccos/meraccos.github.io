f02ab8cc0c588f0412e6c1d69eaa39a0cfd83df8
Meraj <meraccos@unist.ac.kr>
2023-10-06 17:31:49 +0900

    softmax implemented

--- bigram.js
+++ bigram.js
@@ -2,11 +2,9 @@ async function forward(x) {
     const session = await ort.InferenceSession.create('/model.onnx');
     const feeds = { "input": x };
     const results = await session.run(feeds);
-    const output = results[13].data
-    // console.log(output)
+    const logits = results[13].data
     
-    const maxIndex = output.indexOf(Math.max(...output));
-    return maxIndex
+    return logits
 }
 
 async function readCharacterEncodingFile() {
@@ -24,6 +22,34 @@ async function readCharacterEncodingFile() {
     return characterDictionary;
 }
 
+
+function softmax(logits) {
+    console.log(logits)
+    var exp_logits = [];
+    var sum_exp_logits = 0.0;
+    var exp_logit = 0.0;
+    for (let j = 0; j < logits.length; j++) {
+        exp_logit = Math.pow(Math.E, logits[j])
+        exp_logits.push(exp_logit);
+        sum_exp_logits += exp_logit;
+    }
+
+    var probs = [];
+    for (let j = 0; j < logits.length; j++) {
+        probs.push(exp_logits[j] / sum_exp_logits)
+    }
+
+    var sum_probs = 0.0
+    for (let j = 0; j < probs.length; j++) {
+        sum_probs += probs[j]
+    }
+}
+
+function multinomial(logits){
+    probs = softmax(logits)
+    
+}
+
 function generateText() {
     const inputElement = document.getElementById("inputText");
     const userInput = inputElement.value;
@@ -35,16 +61,18 @@ function generateText() {
 
         for (let j = 0; j < userInput; j++) {
             const tensorA = new ort.Tensor("int32", dataA, []);
-            const maxIndex = await forward(tensorA);
+            const logits = await forward(tensorA);
+            const sampled_idx = multinomial(logits);
+            // inefficient
             const characterDictionary = await readCharacterEncodingFile();
 
-            const maxChar = characterDictionary[maxIndex];
-            console.log("Max character:", maxChar);
+            const sampled_char = characterDictionary[sampled_idx];
+            // console.log("Max character:", sampled_char);
 
-            generatedText += maxChar;
+            generatedText += sampled_char;
             outputElement.innerHTML = generatedText;
 
-            dataA = Int32Array.from([maxIndex]);
+            dataA = Int32Array.from([sampled_idx]);
         }
     })();
 }
