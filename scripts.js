789bcc095fe6d3c586f0f2a662ca47ca6e37d08d
Meraj <meraccos@unist.ac.kr>
2023-10-07 01:23:34 +0900

    overflow handled

--- scripts.js
+++ scripts.js
@@ -3,20 +3,18 @@ const body = document.body;
 const inputText = document.getElementById('inputText');
 const outputBox = document.getElementById('output');
 const generatorButton = document.getElementById('generate_button');
-const generator = document.getElementById('generator')
 
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked) {
         body.classList.add('white-mode');
         inputText.classList.add('white-mode');
         outputBox.classList.add('white-mode');
-        generator.classList.add('white-mode');
+        generatorButton.classList.add('white-mode');
     } else {
         body.classList.remove('white-mode');
         inputText.classList.remove('white-mode');
         outputBox.classList.remove('white-mode');
         generatorButton.classList.remove('white-mode');
-        generator.classList.add('white-mode');
     }
 });
 
