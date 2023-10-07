e8be918faf84745f937390eafb09a1c9f8819504
Meraj <meraccos@unist.ac.kr>
2023-10-07 13:36:03 +0900

    division lines white mode added

--- js/common.js
+++ js/common.js
@@ -3,7 +3,11 @@ const body = document.body;
 const inputText = document.getElementById('inputText');
 const outputBox = document.getElementById('output');
 const generatorButton = document.getElementById('generate_button');
-const nizamiButton = document.getElementsByClassName('nizami-button')
+const nizamiButton = document.getElementById('nizami-button')
+const shakespeareButton = document.getElementById('shakespeare-button')
+const dt_ds = document.getElementById('dt_ds')
+const dt_g = document.getElementById('dt_g')
+
 
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked) {
@@ -12,12 +16,18 @@ darkModeToggle.addEventListener('change', () => {
         outputBox.classList.add('white-mode');
         generatorButton.classList.add('white-mode');
         nizamiButton.classList.add('white-mode');
+        shakespeareButton.classList.add('white-mode');
+        dt_ds.classList.add('white-mode');
+        dt_g.classList.add('white-mode');
     } else {
         body.classList.remove('white-mode');
         inputText.classList.remove('white-mode');
         outputBox.classList.remove('white-mode');
         generatorButton.classList.remove('white-mode');
         nizamiButton.classList.remove('white-mode');
+        shakespeareButton.classList.remove('white-mode');
+        dt_ds.classList.remove('white-mode');
+        dt_g.classList.remove('white-mode');
     }
 });
 
