aae928dec43d65cd627bcbbb7f6ac05bd35335a4
Meraj <meraccos@unist.ac.kr>
2023-11-26 00:12:26 +0900

    convert the vals to an array

--- movie/movie.js
+++ movie/movie.js
@@ -1,11 +1,18 @@
 var sliders = document.querySelectorAll(".slider")
 var values = document.querySelectorAll('.value')
+var ValuesArray = []
 
 for (let i = 0; i < sliders.length; i++) {
-    values[i].innerHTML = sliders[i].value / 100;
+    values[i].innerHTML = (sliders[i].value / 100).toFixed(2);
+    ValuesArray.push(sliders[i].value / 100);
 
     sliders[i].oninput = function() {
         values[i].innerHTML = (this.value / 100).toFixed(2);
+        ValuesArray[i] = sliders[i].value / 100;
     };
 }
+
+// var intervalId = setInterval(function() {
+//     console.log(ValuesArray);
+// }, 1000);
 
