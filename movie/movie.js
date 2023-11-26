6fc14c8167b223f168edcbf6d42e07b9efb5344e
Meraj <meraccos@unist.ac.kr>
2023-11-26 11:31:59 +0900

    generate button, recommendation system added

--- movie/movie.js
+++ movie/movie.js
@@ -1,7 +1,10 @@
+// Select the slider, value and output elements
+var outputs = document.querySelectorAll('.output');
 var sliders = document.querySelectorAll(".slider")
 var values = document.querySelectorAll('.value')
 var ValuesArray = []
 
+// fill value elements with slider values and array with the values
 for (let i = 0; i < sliders.length; i++) {
     values[i].innerHTML = (sliders[i].value / 100).toFixed(2);
     ValuesArray.push(sliders[i].value / 100);
@@ -12,7 +15,52 @@ for (let i = 0; i < sliders.length; i++) {
     };
 }
 
-// var intervalId = setInterval(function() {
-//     console.log(ValuesArray);
-// }, 1000);
+// Function for reading the movie names
+async function readCharacterEncodingFile() {
+    const response = await fetch('names.txt');
+    const data = await response.text();
+    const lines = data.split('\n');
+    return lines;
+}
 
+
+
+
+
+// main things going on here
+async function main() {
+    try {
+        // Read the movie names file
+        const movie_names = await readCharacterEncodingFile();
+
+        // Infer the model 
+        var data = Float32Array.from(ValuesArray);
+        const tensor = new ort.Tensor("float32", data, [18]); 
+        
+        const session = await ort.InferenceSession.create('/movie/model.onnx');
+        const feeds = {"onnx::MatMul_0": tensor};
+        const run_out = await session.run(feeds);
+        const output = run_out[5]['data']
+        
+        // Extract top 10 indices
+        let indices = Array.from({ length: output.length }, (_, i) => i);
+        indices.sort((a, b) => output[b] - output[a]);
+        let top10Indices = indices.slice(0, 10);
+        
+        // Extract movie names from indices and print the result
+        let results = ''
+        for (let i = 0; i < top10Indices.length; i++) {
+            for (let j = 0; j < movie_names.length; j++) {
+                if (j == top10Indices[i]) {
+                    outputs[i].innerHTML = movie_names[j]
+                    results += movie_names[j]
+                    results += '<br>'
+                    
+                }
+            }  
+        }
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
